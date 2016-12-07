ee48c0d3d8cfbe3896efb778469e2b6a04815a01
GabrielVerduzco <verduzcogab@gmail.com>
2016-12-07 11:53:30 -0600

    get values of table (just inside values)

--- JS/personal_index.js
+++ JS/personal_index.js
@@ -15,6 +15,7 @@ function generate_table(){
             tbody.childNodes[i].appendChild(document.createElement('td'));
             tbody.childNodes[i].childNodes[j].appendChild(document.createTextNode('0'));
             tbody.childNodes[i].childNodes[j].setAttribute( "class", "dead" );
+            tbody.childNodes[i].childNodes[j].setAttribute( "id", i+"-"+j );
             tbody.childNodes[i].childNodes[j].setAttribute("onclick","resurrect("+i+","+j+")");
          }
     }
@@ -23,6 +24,32 @@ function generate_table(){
 
 function resurrect(x,y){
     tbody.childNodes[x].childNodes[y].textContent="1";
+
     tbody.childNodes[x].childNodes[y].setAttribute( "class", "live" );
  //   console.log('Cordenadas'+x+','+y);
+}
+
+
+function play(){
+    var size = $('#size').val();
+    
+   for(var i=0;i<size;i++){
+		for(var j=0;j<size;j++)
+		{
+            if(i==0 || j==0 ||i==size-1 || j==size-1){
+                
+
+            }
+            else
+            {
+            
+                console.log(i,j);
+               
+            
+            }
+
+            
+        }
+   }
+
 }
