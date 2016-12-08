a73d84c1c3b81d01d35a10e54f83eb9333049600
GabrielVerduzco <verduzcogab@gmail.com>
2016-12-08 17:48:04 -0600

    Toggle and corners

--- JS/personal_index.js
+++ JS/personal_index.js
@@ -1,6 +1,8 @@
 var timeoutID;
 var tbody=document.createElement('tbody');
 var timerId;
+var to
+
 function timer(){
      timerId= setInterval(function(){ play();}, 3000);
 }
@@ -12,6 +14,7 @@ function stop(){
 
 function generate_table(){
     var size = parseInt($('#size').val());
+    size = size +2;
     var table=document.createElement('table');
     table.appendChild(tbody);
     table.setAttribute("class","table table-bordered");
@@ -23,41 +26,83 @@ function generate_table(){
         {
             tbody.childNodes[i].appendChild(document.createElement('td'));
             tbody.childNodes[i].childNodes[j].appendChild(document.createTextNode('0'));
+
             tbody.childNodes[i].childNodes[j].setAttribute( "class", "dead" );
             tbody.childNodes[i].childNodes[j].setAttribute( "id", i+"-"+j);
             tbody.childNodes[i].childNodes[j].setAttribute("onclick","resurrect("+i+","+j+")");
          }
     }
+    document.getElementById("generate");
+    document.getElementById("size");
     document.getElementById("generate").disabled = true;
     document.getElementById("size").disabled = true;
+    hideTable(size);
     
 }
+
+function hideTable(size){
+     
+     for (var i = 0; i <size ; i++) 
+     { 
+          
+          for (var j = 0; j <size ; j++) {
+              
+              tbody.childNodes[i].childNodes[0].setAttribute("class", 'border');
+              $("#"+i+"-"+'0').removeClass('dead');
+              tbody.childNodes[j].childNodes[size-1].setAttribute("class", 'border');
+               $("#"+j+"-"+size-1).removeClass('dead');
+              
+              
+          }
+     }
+     tbody.childNodes[0].setAttribute("class", 'border');
+     $().removeClass('dead');
+     tbody.childNodes[size-1].setAttribute("class", 'border');
+     $().removeClass('dead');
+     
+   
+     
+ 
+ }
 
 
 function resurrect(x,y){
-    tbody.childNodes[x].childNodes[y].textContent="1";
-    tbody.childNodes[x].childNodes[y].setAttribute( "class", "live" );
+ 
+    if  (tbody.childNodes[x].childNodes[y].textContent=="0" && tbody.childNodes[x].childNodes[y].classList.contains('dead')) 
+    {
+        
+        tbody.childNodes[x].childNodes[y].textContent="1";
+        tbody.childNodes[x].childNodes[y].setAttribute( "class", "live" );
+    } 
+    else 
+    {
+        $("#"+x+"-"+y).html('0').removeClass('live').addClass('dead');
+    }
+    
 }
-
 
 function play(){
     
-   
     var size = $('#size').val();
     var cells = [];
+    var limitDownI=0, limitDownJ=0, limitUpI=0, limitUpJ=0; 
     for(var i=0;i<size;i++)
     {
          cells[i] = [];
 		for(var j=0;j<size;j++)
 		{            
-            if(i==0 || j==0 ||i==size-1 || j==size-1){}
+            if(i==0 || j==0 ||i==size-1 || j==size-1){
+                
+            }
             else
             {
-                var limitDownI=0, limitDownJ=0, limitUpI=0, limitUpJ=0;     
                 limitDownI = i+1;
                 limitDownJ = j+1;
                 limitUpI = i-1;
-                limitUpJ = j-1;
+                limitUpJ = j-1;  
+                
+            }
+          
                 var stateCell2 = $("#"+i+"-"+j).attr('class');
                 var auxSum=0;
                 for(var x=limitUpI;x<=limitDownI;x++)
@@ -71,7 +116,6 @@ function play(){
                 }
                 auxSum-=parseInt(tbody.childNodes[i].childNodes[j].textContent);
                 cells[i][j]=auxSum;
-            }
             
         }
    }
